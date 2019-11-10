fbd0bb5f3894f06f3101a57bab95a95eac5af788
ingsnow99 <gohth-wm17@student.tarc.edu.my>
2019-11-10 12:29:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.aboutme
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName:MyName = MyName("Goh Tai Heng")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.myName = myName
+        binding.doneButton.setOnClickListener {
+            addNickname()
+        }
+        binding.nicknameText.setOnClickListener {
+            updateNickname()
+        }
+    }
+    private fun addNickname(){
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.doneButton.windowToken, 0)
+    }
+    private fun updateNickname(){
+        binding.apply {
+            binding.nicknameEdit.visibility = View.VISIBLE
+            binding.doneButton.visibility = View.VISIBLE
+            binding.nicknameText.visibility = View.GONE
+        }
+        binding.nicknameEdit.requestFocus()
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.nicknameEdit,0)
+    }
+}
---
+++ app/src/main/java/com/example/aboutme/MyName.kt
@@ -0,0 +1,2 @@
+package com.example.aboutme
+data class MyName(var name: String = "", var nickname: String = "")
